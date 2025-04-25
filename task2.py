c7eff7ff213e7ba1774dfaba25fa9a424c6bd10d
Anzam2000 <andrejzamaraev29@gmail.com>
2025-04-25 16:41:07 +0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,36 +1,23 @@
-print('narayana')
-def narayana(arr, length):
-    result = []
-    from itertools import combinations
-    for combo in combinations(arr, length):
-        current = list(combo)
-        print(current)
-        result.append(current.copy())
-        while True:
-            index_j = -1
-            for i in range(length - 2, -1, -1):
-                if current[i] < current[i + 1]:
-                    index_i = i
-                    break
-            else:
-                break
-            for j in range(length - 1, -1, -1):
-                if current[j] > current[index_i]:
-                    index_j = j
-                    break
-            current[index_i], current[index_j] = current[index_j], current[index_i]
-            current[index_i + 1:] = current[:index_i:-1]
-            print(current)
-            result.append(current.copy())
-    return result
-list1 = [1, 2, 3]
-list1.sort()
-permutations = narayana(list1, 2)
-print(len(permutations))
-print('itertools')
-import itertools
-a = 0
-for p in itertools.permutations(list1, 2):
-    a += 1
-    print(p)
-print(a)
+
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, cash):
+        if cash > 0:
+            self.__balance += cash
+        else:
+            print("сумма должны быть положительна")
+    def withdraw(self, cash):
+        if cash <= 0:
+            print("Сумма для снятия должна быть положительной")
+        elif cash > self.__balance:
+            print("Недостаточно средств на счете")
+        else:
+            self.__balance -= cash
+    @property
+    def get_balance(self):
+        return self.__balance
+    @get_balance.setter
+    def get_balance(self, value):
+        self.__balance = value
