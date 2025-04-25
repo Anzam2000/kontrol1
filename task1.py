58fe879a4022282fe27101207a3f42740d1259fa
Anzam2000 <andrejzamaraev29@gmail.com>
2025-04-25 16:40:51 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,36 +1,14 @@
-print('narayana')
-def narayana(arr, length):
-    result = []
-    from itertools import combinations
-    for combo in combinations(arr, length):
-        current = list(combo)
-        print(current)
-        result.append(current.copy())
-        while True:
-            index_j = -1
-            for i in range(length - 2, -1, -1):
-                if current[i] < current[i + 1]:
-                    index_i = i
-                    break
-            else:
-                break
-            for j in range(length - 1, -1, -1):
-                if current[j] > current[index_i]:
-                    index_j = j
-                    break
-            current[index_i], current[index_j] = current[index_j], current[index_i]
-            current[index_i + 1:] = current[:index_i:-1]
-            print(current)
-            result.append(current.copy())
-    return result
-list1 = [2, 1, 3, 4]
-list1.sort()
-permutations = narayana(list1, 3)
-print(len(permutations))
-print('itertools')
-import itertools
-a = 0
-for p in itertools.permutations(list1, 3):
-    a += 1
-    print(p)
-print(a)
+
+class Animal:
+    def make_sound(self):
+        pass
+class Dog(Animal):
+    def make_sound(self):
+        print('Собака лает')
+class Cat(Animal):
+    def make_sound(self):
+        print('Кошка мяукает')
+cat = Cat()
+dog = Dog()
+cat.make_sound()
+dog.make_sound()
